f84e3d1f3e5d49edb4f0e0ccce1ed35e9f7cf898
Denis Cirtiu <deniscirtiu@gmail.com>
2019-05-27 20:58:38 +0300

    #6 Animatiii :D

--- app/src/main/java/com/example/easygame/Constants.java
+++ app/src/main/java/com/example/easygame/Constants.java
@@ -1,6 +1,9 @@
 package com.example.easygame;
 
+import android.content.Context;
+
 public class Constants {
     public static int SCREEN_WIDTH;
     public static int SCREEN_HEIGHT;
+    public static Context CURRENT_CONTEXT;
 }
